66c525533d5c495d55042c4bdf209b00d4d740f2
Cpm_jhon <130876841+CpmJhon@users.noreply.github.com>
2025-01-02 03:19:15 +0700

    module.js

---
+++ module.js
@@ -0,0 +1,35 @@
+/*
+    REREZZ OFFICIAL [ DEVELOPER && FIX BASE ]
+    DGXEON & HW [ BASE ]
+    
+    TQTO
+        ALLAH SWT
+        MUHAMMAD SAW
+        MY PARENTS
+        ALL DEVELOPER BOT
+        MODULE AND API PROVIDERS
+    TQTO FRIEND
+        IKKY OFFC [ MY FRIEND ]
+        SANJAYA [ MY FRIEND ] 
+        NERIXKIRA [ MY FRIEND ]
+    MY TEAM 
+        REREZZ OFFICIAL DEV [ DEVELOPER ]
+        TRO DEV [ FEATURE IDE ]
+        RESTU [ EDITOR ]
+*/
+
+module.exports = {
+modul: {
+	axios: require('axios'),
+	boom: require('@hapi/boom'),
+	baileys: require('@whiskeysockets/baileys'), 
+	chalk: require('chalk'),
+	cheerio: require('cheerio'),
+	child_process: require('child_process'),
+	fs: require('fs'),
+	fetch: require('node-fetch'),
+	FormData: require('form-data'),
+	FileType: require('file-type'),
+	process: require('process'),
+	PhoneNumber: require('awesome-phonenumber')
+}}
